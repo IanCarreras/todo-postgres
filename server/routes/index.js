21cf6d6aef4759b9e230b8ea2454086d9fe38a56
Ian <mus5002@aol.com>
2018-08-21 07:03:22 -0400

    added methods to the todos controller and added a todoitems controller

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,16 @@
 const { todos: todosController } = require('../controllers');
+const { todoItems: todoItemsController } = require('../controllers')
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
     message: 'Welcome to the Todos API!',
   }));
 
-  app.post('/api/todos', todosController.create);
+  app.post('/api/todos', todosController.create)
+  app.get('/api/todos', todosController.list)
+  app.get('/api/todos/:todoId', todosController.retrieve)
+  app.put('/api/todos/:todoId', todosController.update)
+  app.delete('api/todos/:todoId', todosController.destroy)
+
+  app.post('/api/todos/:todoId/items', todoItemsController.create)
 };
