c5f2327fe7e02130d1431710e485d5e48fe23382
Ian <mus5002@aol.com>
2018-08-26 11:10:43 -0400

    fixed delete route

--- server/controllers/todoitems.js
+++ server/controllers/todoitems.js
@@ -1,36 +1,36 @@
 const { TodoItem } = require('../models')
 
 module.exports = {
-  create(req, res) {
-    return TodoItem
-    .create({
-      content: req.body.content,
-      todoId: req.params.todoId,
-    })
-    .then(todoItem => res.status(201).send(todoItem))
-    .catch(error => res.status(400).send(error))
-  },
-  update(req, res) {
-    return TodoItem
-      .find({
-          where: {
-            id: req.params.todoItemId,
-            todoId: req.params.todoId
-          }
-        })
-      .then(todoItem => {
-        if (!todoItem) {
-          return res.status(404).send({
-            message: 'TodoItem Not Found'
-          })
-        }
-
-        return todoItem
-          .update(req.body, { fields: Object.keys(req.body) })
-          .then(updatedTodoItem => res.status(200).send(updatedTodoItem))
-          .catch(error => res.status(400).send(error))
+  async create(req, res) {
+    try {
+      const todoItem = await Todoitem.create({
+        content: req.body.content,
+        todoId: req.params.todoId,
       })
-      .catch(error => res.status(400).send(error))
+
+      res.status(201).send(todoItem)
+    } catch(err) {
+      res.status(400).send(error)
+    }
+  },
+  async update(req, res) {
+    try {
+      const todoItem = await TodoItem.find({
+        where: {
+          id: req.params.todoItemId,
+          todoId: req.params.todoId
+        }
+      });
+      if (!todoItem) {
+        return res.status(404).send({
+          message: 'TodoItem Not Found'
+        })
+      }
+      const updatedTodoItem = await todoItem.update(req.body, { fields: Object.keys(req.body) });
+      res.status(200).send(updatedTodoItem);
+    } catch(err) {
+      res.status(400).send(err);
+    }
   },
   destroy(req, res) {
     return TodoItem
